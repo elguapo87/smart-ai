0a2e930889587bf485ba3dae4f24a9c39b4b8fad
elguapo87 <pglogovac@gmail.com>
2025-05-05 23:22:03 +0200

    Added & created userModel for storage clerk users in MongoDB

---
+++ src/models/userModel.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: false
+    }
+}, { timestamps: true });
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
