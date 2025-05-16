18558e5ada2742f90fa5d25daa812a28d55f59ec
elguapo87 <pglogovac@gmail.com>
2025-05-17 00:53:18 +0200

    Fixed chat name on top of the screen for smaller screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,13 +78,13 @@ export default function HomePage() {
               <div className="flex items-center gap-3">
                 {
                   token
-                  ? 
-                  <div className="relative group">
-                    <Image src={assets.profile_icon} alt="Profile Icon" className="w-7 h-7 rounded-full" />
-                    <button onClick={logoutHandler} className={`border-none absolute top-[28px] right-0 hidden text-sm font-semibold px-1.5 py-0.5 group-hover:block ${isDark ? "bg-stone-200 text-gray-800" : "bg-gray-800 text-stone-200"} rounded transition-all duration-300 cursor-pointer`}>Logout</button>
-                  </div>
-                  :  
-                  <button onClick={() => setUserLogin(true)} className={`border-none text-sm px-2.5 py-1 rounded bg-primary text-white hover:bg-purple-800 transition-all duration-300 cursor-pointer`}>Login</button>
+                    ?
+                    <div className="relative group">
+                      <Image src={assets.profile_icon} alt="Profile Icon" className="w-7 h-7 rounded-full" />
+                      <button onClick={logoutHandler} className={`border-none absolute top-[28px] right-0 hidden text-sm font-semibold px-1.5 py-0.5 group-hover:block ${isDark ? "bg-stone-200 text-gray-800" : "bg-gray-800 text-stone-200"} rounded transition-all duration-300 cursor-pointer`}>Logout</button>
+                    </div>
+                    :
+                    <button onClick={() => setUserLogin(true)} className={`border-none text-sm px-2.5 py-1 rounded bg-primary text-white hover:bg-purple-800 transition-all duration-300 cursor-pointer`}>Login</button>
                 }
                 <ThemeToggle />
               </div>
@@ -93,36 +93,38 @@ export default function HomePage() {
 
           {
             messages.length === 0
-                 ?
-            (<>
-              <div className="flex items-end">
-                <Image src={assets.bulb_ai} alt="" className={`h-16 w-16 aspect-square ${isDark ? "invert" : ""}`} />
-                <p className="text-2xl font-medium">Hi, I&apos;m Smart AI</p>
-              </div>
-              <p className="text-sm mt-2">How can I help you today?</p>
-            </>)
-                 :
-            (<div ref={containerRef} className="relative flex flex-col items-center justify-start w-full mt-20 max-h-screen overflow-y-auto">
-              <p className="fixed top-16 sm:top-8 border border-transparent hover:border-gray-500/50 px-2 py-1 rounded-lg font-semibold mb-6">
-                {token && selectedChat?.name}
-              </p>
-
-              {messages.map((msg, index) => (
-                <Message key={msg._id ?? index} message={msg} />
-              ))}
-
-              {isLoading && (
-                <div className="flex gap-2.5 max-w-3xl w-full py-3">
-                  <Image src={assets.bulb_ai} alt="Logo" className={`h-9 w-9 p-1 border rounded-full ${isDark ? "border-black/15 invert" : "border-gray-600/50"}`} />
-                  <div className="loader flex justify-center items-center gap-1">
-                    <div className="w-1 h-1 rounded-full bg-gray-600 dark:bg-white animate-bounce"></div>
-                    <div className="w-1 h-1 rounded-full bg-gray-600 dark:bg-white animate-bounce"></div>
-                    <div className="w-1 h-1 rounded-full bg-gray-600 dark:bg-white animate-bounce"></div>
-                  </div>
+              ?
+              (<>
+                <div className="flex items-end">
+                  <Image src={assets.bulb_ai} alt="" className={`h-16 w-16 aspect-square ${isDark ? "invert" : ""}`} />
+                  <p className="text-2xl font-medium">Hi, I&apos;m Smart AI</p>
                 </div>
-              )}
+                <p className="text-sm mt-2">How can I help you today?</p>
+              </>)
+              :
+              (<div ref={containerRef} className="relative flex flex-col items-center justify-start w-full mt-20 max-h-screen overflow-y-auto">
+                <p className="fixed top-8 border border-transparent hover:border-gray-500/50 px-2 py-1 rounded-lg font-semibold mb-6 max-w-[70%] overflow-hidden text-ellipsis whitespace-nowrap">
+                  {token && selectedChat?.name && selectedChat.name.length > 10
+                    ? selectedChat.name.slice(0, 10) + "…"
+                    : selectedChat?.name}
+                </p>
 
-            </div>)
+                {messages.map((msg, index) => (
+                  <Message key={msg._id ?? index} message={msg} />
+                ))}
+
+                {isLoading && (
+                  <div className="flex gap-2.5 max-w-3xl w-full py-3">
+                    <Image src={assets.bulb_ai} alt="Logo" className={`h-9 w-9 p-1 border rounded-full ${isDark ? "border-black/15 invert" : "border-gray-600/50"}`} />
+                    <div className="loader flex justify-center items-center gap-1">
+                      <div className="w-1 h-1 rounded-full bg-gray-600 dark:bg-white animate-bounce"></div>
+                      <div className="w-1 h-1 rounded-full bg-gray-600 dark:bg-white animate-bounce"></div>
+                      <div className="w-1 h-1 rounded-full bg-gray-600 dark:bg-white animate-bounce"></div>
+                    </div>
+                  </div>
+                )}
+
+              </div>)
           }
 
           <PromptBox isLoading={isLoading} setIsLoading={setIsLoading} />
